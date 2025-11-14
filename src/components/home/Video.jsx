62de8e6198476f6551e56b1c99a3ced4b31d54d8
parth <parthpipermintwala9@gmail.com>
2025-11-15 00:27:33 +0530

    viedo

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import heroVideo from "../../assets/Video/HeroVideo.mp4";
+import Loading from "../common/Loading";
 
 const Video = ({ rounded }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <video
-      autoPlay
-      loop
-      muted
-      className={`h-full w-full object-cover ${rounded ? "rounded-full" : ""}`}
-      src={heroVideo}
-      preload="none"
-      disablePictureInPicture
-      controlsList="nofullscreen nodownload noremoteplayback noplaybackrate nofullscreen"
-      playsInline
-    ></video>
+    <div className="relative h-full w-full">
+      {isLoading && <Loading />}
+      <video
+        autoPlay
+        loop
+        muted
+        className={`h-full w-full object-cover ${
+          rounded ? "rounded-full" : ""
+        } ${
+          isLoading ? "opacity-0" : "opacity-100"
+        } transition-opacity duration-500`}
+        src={heroVideo}
+        preload="auto"
+        disablePictureInPicture
+        controlsList="nofullscreen nodownload noremoteplayback noplaybackrate nofullscreen"
+        playsInline
+        onLoadedData={() => setIsLoading(false)}
+      ></video>
+    </div>
   );
 };
 export default Video;
